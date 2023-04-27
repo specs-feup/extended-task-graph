a04d74f2c7e78ee3f711603198701a33bf555e74
tiagolascasas <tiagolascasas@outlook.com>
2023-04-27 01:43:59 +0100

    Reduce to subset stage working for first scenario

--- src/preprocessing/SubsetReducer.js
+++ src/preprocessing/SubsetReducer.js
@@ -1,11 +1,28 @@
 "use strict";
 
 laraImport("weaver.Query");
+laraImport("clava.opt.NormalizeToSubset");
+laraImport("clava.opt.PrepareForInlining");
+laraImport("clava.code.Inliner");
+laraImport("clava.code.StatementDecomposer");
 
 class SubsetReducer {
     constructor() { }
 
     reduce() {
+        this.normalizeToSubset();
+        this.decomposeStatements();
+    }
 
+    normalizeToSubset() {
+        NormalizeToSubset(Query.root(), { simplifyLoops: { forToWhile: false } });
+    }
+
+    decomposeStatements() {
+        const decomp = new StatementDecomposer();
+
+        for (var stmt of Query.search("statement", { isInsideHeader: false })) {
+            decomp.decomposeAndReplace(stmt);
+        }
     }
 }
