c1e4e7232af5d15e532472290a404ec265a92fc1
tiagolascasas <tiagolascasas@outlook.com>
2023-09-09 04:07:58 +0100

    Better stresstest for loops and if/else blocks

--- test/inputs/stresstest/stresstest.cpp
+++ test/inputs/stresstest/stresstest.cpp
@@ -5,25 +5,7 @@
 
 using namespace std;
 
-void funA(int A[N], int D[N * M], int E[N * M])
-{
-    for (int i = 0; i < N; i++)
-    {
-        A[i] = D[i * M + i];
-        A[i] = E[i * M + i];
-    }
-}
-
-void funB(int A[N], int D[N * M], int E[N * M])
-{
-    for (int i = 0; i < N; i++)
-    {
-        A[i] = D[i * M + i];
-        A[i] = E[i * M + i];
-    }
-}
-
-void funC(int A[N], int B[M])
+void useB_defineA(int A[N], int B[M])
 {
     for (int i = 0; i < N; i++)
     {
@@ -31,39 +13,23 @@ void funC(int A[N], int B[M])
     }
 }
 
-void funD(int A[N], int B[M], int C[N])
+void useA_defineB(int A[N], int B[M])
 {
     for (int i = 0; i < N; i++)
     {
-        A[i] = B[i % M];
-        A[i] = C[i];
+        B[i] = A[i % M];
     }
 }
 
-void funE(int A[N], int B[M], int C[N])
+void useAB_defineC(int A[N], int B[M], int C[N])
 {
     for (int i = 0; i < N; i++)
     {
-        A[i] = B[i % M];
-        A[i] = C[i];
+        C[i] = B[i % M];
     }
-}
-
-void funF(int A[N], int B[M], int C[N])
-{
     for (int i = 0; i < N; i++)
     {
-        A[i] = B[i % M];
-        A[i] = C[i];
-    }
-}
-
-void funG(int A[N], int B[M], int C[N])
-{
-    for (int i = 0; i < N; i++)
-    {
-        A[i] = B[i % M];
-        A[i] = C[i];
+        C[i] = A[i % M];
     }
 }
 
@@ -72,33 +38,65 @@ void app_start(int A[N], int B[M], int C[N])
     int D[N * M] = {0};
     int E[N * M] = {0};
 
-    funA(A, D, E);
-    funB(A, D, A);
+    // an innocent first task
+    useB_defineA(A, B);
 
-    // Case A: task is repeated N times
+    // Case L1: task in a loop
     for (int i = 0; i < N; i++)
     {
-        funC(A, B);
+        useB_defineA(A, B);
     }
+    useAB_defineC(A, B, C); // require both A and B, to serve as a "stopgap" between each case
 
+    // Case L2: multiple tasks in a loop, with dependencies
     for (int i = 0; i < N; i++)
     {
-        funD(A, B, C);
+        useB_defineA(A, B);
+        useA_defineB(A, B);
+    }
+    useAB_defineC(A, B, C);
+
+    // Case L3: loop nesting with calls at different levels
+    for (int i = 0; i < N; i++)
+    {
+        useA_defineB(A, B);
+        // L1 repeated
         for (int j = 0; j < M; j++)
         {
-            funE(A, B, C);
+            useB_defineA(A, B);
         }
-        funD(A, B, C);
+        useA_defineB(A, B);
     }
+    useAB_defineC(A, B, C);
 
+    // Case C1: single conditional
+    if (A[0] == 1)
+    {
+        useB_defineA(A, B);
+    }
+    useAB_defineC(A, B, C);
+
+    // Case C2: if/else, both modifying the same data
     if (A[1] == 2)
     {
-        funF(A, B, C);
+        useB_defineA(A, B);
     }
     else
     {
-        funG(A, B, C);
+        useB_defineA(A, B);
     }
+    useAB_defineC(A, B, C);
+
+    // Case C3: if/else, both modifying different data
+    if (A[2] == 3)
+    {
+        useB_defineA(A, B);
+    }
+    else
+    {
+        useA_defineB(A, B);
+    }
+    useAB_defineC(A, B, C);
 }
 
 int main()
