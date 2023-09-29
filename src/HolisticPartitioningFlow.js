244c1b8ee51f70b258be66373c3794e20504dcdf
tiagolascasas <tiagolascasas@outlook.com>
2023-09-29 22:37:24 +0100

    UPT using the paths provided by tester

--- src/HolisticPartitioningFlow.js
+++ src/HolisticPartitioningFlow.js
@@ -43,8 +43,7 @@ class HolisticPartitioningFlow extends UPTStage {
         const estimDir = this.getOutputDir() + "/estimations";
 
         const annotator = new TaskGraphAnnotator(this.getTopFunction(), estimDir, this.getAppName());
-        annotator.annotateCpuEstimations(tg, "none");
-        annotator.annotateFpgaEstimations(tg, "none");
+        annotator.annotateAll(tg, this.#config);
 
         this.log("Task graph successfully annotated with CPU/FPGA estimations!");
     }
--- src/estimation/TaskGraphAnnotator.js
+++ src/estimation/TaskGraphAnnotator.js
@@ -3,6 +3,7 @@
 laraImport("UPTStage");
 laraImport("estimation/PerfPrecalcEstimator");
 laraImport("estimation/VitisHlsPrecalcEstimator");
+laraImport("estimation/VitisHlsRealtimeEstimator");
 
 class TaskGraphAnnotator extends UPTStage {
 
@@ -10,13 +11,33 @@ class TaskGraphAnnotator extends UPTStage {
         super("HPFlow-TaskGraphAnnotator", topFunction, outputDir, appName);
     }
 
+    annotateAll(taskGraph, config) {
+        const cpuEstim = config["cpuEstim"];
+        this.annotateCpuEstimations(taskGraph, cpuEstim);
+
+        if (config["useHls"]) {
+            const targetPart = config["targetPart"];
+            const period = config["period"];
+            this.annotateFpgaEstimationsRealtime(taskGraph, targetPart, period);
+        }
+        else {
+            const fpgaEstim = config["fpgaEstim"];
+            this.annotateFpgaEstimationsPrecalculated(taskGraph, fpgaEstim);
+        }
+    }
+
     annotateCpuEstimations(taskGraph, profilingResults) {
         const estimator = new PerfPrecalcEstimator(this.getOutputDir(), profilingResults);
         estimator.estimateTaskGraph(taskGraph, true);
     }
 
-    annotateFpgaEstimations(taskGraph, synthesisResults) {
+    annotateFpgaEstimationsPrecalculated(taskGraph, synthesisResults) {
         const estimator = new VitisHlsPrecalcEstimator(this.getOutputDir(), synthesisResults);
+        estimator.estimateTaskGraph(taskGraph, true);
+    }
+
+    annotateFpgaEstimationsRealtime(taskGraph, targetPart, period) {
+        const estimator = new VitisHlsRealtimeEstimator(this.getOutputDir(), targetPart, period);
         estimator.estimateTaskGraph(taskGraph, true);
     }
 }
