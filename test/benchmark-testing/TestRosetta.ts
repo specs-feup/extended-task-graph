213707a2d32e0124517caed98c2d348e067f035c
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-10-10 15:57:20 -0400

    Creates a test for Rosetta

---
+++ test/benchmark-testing/TestRosetta.ts
@@ -0,0 +1,9 @@
+const apps: Record<string, string[]> = {
+    "3d-rendering-N": ["c++11", "rendering_sw"],
+    "digit-recognition-N": ["c++11", "DigitRec_sw"],
+    "face-detection-N": ["c++11", "face_detect_sw"],
+    "optical-flow-current": ["c++11", "optical_flow_sw"],
+    "optical-flow-sintel": ["c++11", "optical_flow_sw"],
+    "spam-filter-N": ["c++11", "SgdLR_sw", "main"]
+};
+
