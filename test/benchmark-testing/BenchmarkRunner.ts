54cda6e8592065b81fd14daf314bfb263a784a40
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-10-28 17:16:27 -0400

    Separates common benchmark running code into its own file

---
+++ test/benchmark-testing/BenchmarkRunner.ts
@@ -0,0 +1,52 @@
+import { LiteBenchmarkLoader, BenchmarkSuite } from "./LiteBenchmarkLoader.js";
+import { ExtendedTaskGraphAPI } from "../../src/api/ExtendedTaskGraphAPI.js";
+import chalk from "chalk";
+
+export function runEtgForBenchmark(suite: BenchmarkSuite, apps: string[], settings: Record<string, boolean | string>): boolean {
+
+    for (const app of apps) {
+        log(`Running ETG flows for app ${app} of benchmark suite ${suite.name}`);
+        const outputDir = settings.outputDir as string;
+        const cachedPath = `${outputDir}/${app}/src/trans`;
+
+        let invalidCache = false;
+        log(`Trying to load cached app ${app} from ${cachedPath}...`);
+        let topFunctionName = LiteBenchmarkLoader.load(suite, app, cachedPath);
+
+        if (topFunctionName === "<none>") {
+            log(`Could not load cached app ${app}, loading full benchmark instead`);
+            invalidCache = true;
+
+            log(`Loading full benchmark for app ${app}...`);
+            topFunctionName = LiteBenchmarkLoader.load(suite, app);
+            if (topFunctionName === "<none>") {
+                log(`Could not load app ${app}, skipping...`);
+                return false;
+            }
+            log(`Loaded full benchmark for app ${app} with top function ${topFunctionName}`);
+        }
+        else {
+            log(`Loaded cached app ${app} with top function ${topFunctionName}`);
+        }
+        const api = new ExtendedTaskGraphAPI(topFunctionName, outputDir, app);
+
+        if (invalidCache) {
+            const dumpAST = settings.dumpAST as boolean;
+            const doTransformations = settings.doTransformations as boolean;
+            const dumpCallGraph = settings.dumpCallGraph as boolean;
+            api.runCodeTransformationFlow(dumpCallGraph, dumpAST, doTransformations);
+        }
+
+        const generateGraph = settings.generateGraph as boolean;
+        const gatherMetrics = settings.gatherMetrics as boolean;
+        api.runTaskGraphGenerationFlow(generateGraph, gatherMetrics);
+
+        log(`Finished running ETG flows for app ${app} of benchmark suite ${suite.name}`);
+    }
+    return true;
+}
+
+function log(msg: string): void {
+    const header = chalk.magentaBright("BenchmarkRunner");
+    console.log(`[${header}] ${msg}`);
+}
--- test/benchmark-testing/TestRosetta.ts
+++ test/benchmark-testing/TestRosetta.ts
@@ -1,9 +1,18 @@
-import { LiteBenchmarkLoader, BenchmarkSuite } from "./LiteBenchmarkLoader.js";
+import { runEtgForBenchmark } from "./BenchmarkRunner.js";
+import { BenchmarkSuite } from "./LiteBenchmarkLoader.js";
 import { SuiteSelector } from "./SuiteSelector.js";
-import { ExtendedTaskGraphAPI } from "../../src/api/ExtendedTaskGraphAPI.js";
 
 const rosettaSuite: BenchmarkSuite = SuiteSelector.ROSETTA;
-//const apps = rosettaSuite.apps;
+
+const settings = {
+    outputDir: "output/rosetta",
+    dumpAST: true,
+    doTransformations: true,
+    dumpCallGraph: true,
+    generateGraph: true,
+    gatherMetrics: true,
+}
+
 const apps = [
     //"3d-rendering",
     //"digit-recognition",
@@ -12,30 +21,6 @@ const apps = [
     //"spam-filter"
 ];
 
-for (const app of apps) {
-    const outputDir = "output/rosetta";
-    const cachedPath = `${outputDir}/${app}/src/trans`;
 
-    let invalidCache = false;
-    const topFunctionName = LiteBenchmarkLoader.load(rosettaSuite, app, cachedPath);
 
-    if (topFunctionName === "<none>") {
-        console.error(`Could not load cached app ${app}, loading full benchmark instead`);
-        invalidCache = true;
-    }
-    else {
-        console.log(`Loaded cached app ${app} with top function ${topFunctionName}`);
-    }
-    const api = new ExtendedTaskGraphAPI(topFunctionName, outputDir, app);
-
-    if (invalidCache) {
-        const dumpAST = true;
-        const doTransformations = true;
-        const dumpCallGraph = true;
-        api.runCodeTransformationFlow(dumpCallGraph, dumpAST, doTransformations);
-    }
-
-    const generateGraph = true;
-    const gatherMetrics = true;
-    api.runTaskGraphGenerationFlow(generateGraph, gatherMetrics);
-}
+runEtgForBenchmark(rosettaSuite, apps, settings);
