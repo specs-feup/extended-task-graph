2138721da9e91a708b2c399c2b6124ee121f7b06
tiagolascasas <tiagolascasas@outlook.com>
2025-07-23 03:52:45 +0100

    External call assignment detection is now more generic and resilient

--- src/taskgraph/tasks/ExternalTask.ts
+++ src/taskgraph/tasks/ExternalTask.ts
@@ -1,12 +1,14 @@
-import { Call, FunctionType, Vardecl, Varref } from "@specs-feup/clava/api/Joinpoints.js";
+import { BinaryOp, Call, FunctionType, Vardecl, Varref } from "@specs-feup/clava/api/Joinpoints.js";
 import { ConcreteTask } from "./ConcreteTask.js";
 import { TaskType } from "./TaskType.js";
 import { ExternalTaskDataPolicy } from "./ExternalTaskDataPolicy.js";
 import Query from "@specs-feup/lara/api/weaver/Query.js";
 import { DataItemOrigin } from "../DataItemOrigin.js";
+import { DataItem } from "../dataitems/DataItem.js";
 
 export class ExternalTask extends ConcreteTask {
     private externalTaskDataPolicy: ExternalTaskDataPolicy;
+    private assignedDataItem: DataItem | null = null;
 
     constructor(call: Call, hierParent: ConcreteTask, delimiter = ".") {
         super(TaskType.EXTERNAL, call, hierParent, call.name, delimiter, "TEx");
@@ -16,7 +18,7 @@ export class ExternalTask extends ConcreteTask {
         this.setExternalTaskDataPolicy(ExternalTaskDataPolicy.ALL_READ_WRITE);
     }
 
-    public setExternalTaskDataPolicy(policy: ExternalTaskDataPolicy) {
+    public setExternalTaskDataPolicy(policy: ExternalTaskDataPolicy): void {
         this.externalTaskDataPolicy = policy;
 
         for (const dataItem of this.getData()) {
@@ -33,7 +35,19 @@ export class ExternalTask extends ConcreteTask {
         }
     }
 
-    private populateExternalCallData() {
+    public getExternalTaskDataPolicy(): ExternalTaskDataPolicy {
+        return this.externalTaskDataPolicy;
+    }
+
+    public isAssigned(): boolean {
+        return this.assignedDataItem != null;
+    }
+
+    public getAssignedDataItem(): DataItem | null {
+        return this.assignedDataItem;
+    }
+
+    private populateExternalCallData(): void {
         // TS conversion: original code used varrefs, but we are now using vardecls.
         const refs: Set<Vardecl> = new Set();
         const call = this.getCall();
@@ -48,10 +62,34 @@ export class ExternalTask extends ConcreteTask {
             }
         }
         const vardeclArray: Vardecl[] = Array.from(refs);
+
+        const assignedVardecl = this.getAssignedVardecl(call);
+        if (assignedVardecl != null) {
+            vardeclArray.push(assignedVardecl);
+        }
         this.createDataObjects(vardeclArray, DataItemOrigin.PARAM);
+
+        if (assignedVardecl != null) {
+            this.assignedDataItem = this.getDataItemByName(assignedVardecl?.name || "");
+        }
     }
 
-    public getExternalTaskDataPolicy() {
-        return this.externalTaskDataPolicy;
+    private getAssignedVardecl(call: Call): Vardecl | null {
+        const binaryOp = call.getAncestor("binaryOp") as BinaryOp | null;
+        if (binaryOp == null) {
+            return null;
+        }
+
+        if (binaryOp.operator !== "=") {
+            return null;
+        }
+        if (!(binaryOp.left instanceof Varref)) {
+            return null;
+        }
+        const varref = binaryOp.left as Varref;
+        if (!(varref.vardecl instanceof Vardecl)) {
+            return null;
+        }
+        return varref.vardecl as Vardecl;
     }
 }
