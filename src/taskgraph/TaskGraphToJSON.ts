4309c0a2691bb47adc156648b1acfa346ad95523
tiagolascasas <tiagolascasas@outlook.com>
2025-09-19 03:44:16 +0100

    Improves ETG to JSON conversion by adding types

--- src/taskgraph/TaskGraphToJSON.ts
+++ src/taskgraph/TaskGraphToJSON.ts
@@ -1,3 +1,4 @@
+import { DataItem } from "./dataitems/DataItem.js";
 import { TaskGraph } from "./TaskGraph.js";
 import { ConcreteTask } from "./tasks/ConcreteTask.js";
 import { Task } from "./tasks/Task.js";
@@ -5,49 +6,78 @@ import { Task } from "./tasks/Task.js";
 export class TaskGraphToJSON {
     constructor() { }
 
-    public convert(etg: TaskGraph): object {
-        const json: any = {};
-
+    public convert(etg: TaskGraph): TaskGraphJSON {
         const source = etg.getSourceTask();
         const sink = etg.getSinkTask();
         const globals = etg.getGlobalTask();
-        const top = etg.getTopHierarchicalTask();
+        const top = etg.getTopHierarchicalTask()!;
 
-        json["source"] = {
-            id: source.getId(),
-            name: source.getName()
+        const json: TaskGraphJSON = {
+            source: this.convertTask(source),
+            sink: this.convertTask(sink),
+            globals: this.convertTask(globals),
         };
-        json["sink"] = {
-            id: sink.getId(),
-            name: sink.getName()
-        };
-        json["globals"] = {
-            id: globals.getId(),
-            name: globals.getName()
-        };
-
-        if (top != null) {
-            json["top"] = this.convertTask(top);
-        }
-        else {
-            console.log("[TaskGraphToJSON] No top hierarchical task found, cannot convert to JSON.");
+        if (top) {
+            json.top = this.convertTask(top) as ConcreteTaskJSON;
         }
 
         return json;
     }
 
-    protected convertTask(task: ConcreteTask): object {
-        const json: any = {};
-        json["name"] = task.getName();
-        json["type"] = task.getType();
-        json["call"] = task.getCall() ? task.getCall()?.signature : "N/A";
-        json["id"] = task.getId();
-        json["numTasks"] = task.getHierarchicalChildren().length;
-        json["childTasks"] = [];
-        for (const childTask of task.getHierarchicalChildren()) {
-            const childJson = this.convertTask(childTask);
-            json["childTasks"].push(childJson);
+    protected convertTask(task: Task): TaskJSON {
+        const json: TaskJSON = {
+            name: task.getName(),
+            type: task.getType().toString(),
+            id: task.getId(),
+            dataItems: task.getData().map(di => this.convertDataItem(di))
+        };
+
+        if (task instanceof ConcreteTask) {
+            (json as ConcreteTaskJSON).call = task.getCall() ? task.getCall()!.signature : "N/A";
+            (json as ConcreteTaskJSON).numTasks = 1;
+            (json as ConcreteTaskJSON).childTasks = [];
         }
         return json;
     }
+
+    protected convertDataItem(dataItem: DataItem): DataItemJSON {
+        const json: DataItemJSON = {
+            name: [dataItem.getNameInInterface(), dataItem.getNameInTask(), dataItem.getNameInPreviousTask()].join(":"),
+            origin: dataItem.getItemOriginType().toString(),
+            datatype: dataItem.getDatatype(),
+            size: dataItem.getDatatypeSize(),
+            isWritten: dataItem.isWritten(),
+            isRead: dataItem.isRead(),
+        };
+        return json;
+    }
+}
+
+export type TaskGraphJSON = {
+    source: TaskJSON;
+    sink: TaskJSON;
+    globals: TaskJSON;
+    top?: ConcreteTaskJSON;
+};
+
+export type TaskJSON = {
+    name: string;
+    type: string;
+    id: string;
+    dataItems: DataItemJSON[];
+};
+
+export type ConcreteTaskJSON = TaskJSON & {
+    call: string;
+    numTasks: number;
+    childTasks: TaskJSON[];
+};
+
+export type DataItemJSON = {
+    name: string;
+    origin: string;
+    datatype: string;
+    size: number;
+    isWritten: boolean;
+    isRead: boolean;
 }
