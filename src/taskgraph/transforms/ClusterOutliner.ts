6880de5dd59429d8be92fed73d1018bc7a38d746
tiagolascasas <tiagolascasas@outlook.com>
2025-11-08 01:57:47 +0000

    ClusterOutliner now supports non-flat clusters

--- src/taskgraph/transforms/ClusterOutliner.ts
+++ src/taskgraph/transforms/ClusterOutliner.ts
@@ -2,7 +2,7 @@ import { Outliner } from "@specs-feup/clava-code-transforms/Outliner";
 import { Cluster } from "../Cluster.js";
 import { RegularTask } from "../tasks/RegularTask.js";
 import { TopologicalSort } from "../util/TopologicalSort.js";
-import { BinaryOp, Call, DeclStmt, Expression, FileJp, FunctionJp, Include, Param, ReturnStmt, Scope, Statement, Vardecl, Varref } from "@specs-feup/clava/api/Joinpoints.js";
+import { BinaryOp, Call, DeclStmt, Expression, FileJp, FunctionJp, If, Include, Joinpoint, Loop, Param, ReturnStmt, Scope, Statement, Struct, Switch, TypedefDecl, Vardecl, Varref } from "@specs-feup/clava/api/Joinpoints.js";
 import Clava from "@specs-feup/clava/api/clava/Clava.js";
 import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
 import Query from "@specs-feup/lara/api/weaver/Query.js";
@@ -56,13 +56,12 @@ export class ClusterOutliner extends AStage {
             const derefNeeded = !globalDecl?.type.isPointer && param.type.isPointer;
             if (derefNeeded) {
                 this.log(`  Global ${globalDecl.name} requires dereferencing.`);
+                return ClavaJoinPoints.unaryOp("&", ClavaJoinPoints.varRef(unglobifiedName, globalDecl.type));
             }
             else {
                 this.log(`  Global ${globalDecl.name} passed as-is.`);
+                return ClavaJoinPoints.varRef(unglobifiedName, globalDecl.type);
             }
-            const type = derefNeeded ? ClavaJoinPoints.pointer(globalDecl.type) : globalDecl.type;
-
-            return ClavaJoinPoints.varRef(unglobifiedName, type);
         });
         const bridgeCallArgs = [...bridgeCallBaseArgs, ...bridgeCallGlobalArgs];
         const bridgeCall = ClavaJoinPoints.call(bridgeFun, ...bridgeCallArgs);
@@ -91,20 +90,45 @@ export class ClusterOutliner extends AStage {
     }
 
     private replicateIncludes(sourceFile: FileJp, targetFile: FileJp): void {
+        // add struct defs
+        for (const struct of Query.searchFrom(sourceFile, Struct)) {
+            const structClone = struct.deepCopy() as Struct;
+            targetFile.insertBegin(structClone);
+        }
+        // add typedefs
+        for (const declStmt of Query.searchFrom(sourceFile, DeclStmt, (d) => {
+            if (d.children.length === 0) {
+                return false;
+            }
+            const child = d.children[0];
+            return child instanceof TypedefDecl;
+        })) {
+            const declClone = declStmt.deepCopy() as DeclStmt;
+            targetFile.insertBegin(declClone);
+        }
+        // add includes
         for (const include of Query.searchFrom(sourceFile, Include)) {
             targetFile.addInclude(include.name, include.isAngled);
         }
     }
 
     private buildSelector(swCall: Statement, bridgeCall: Statement): void {
-        const ifDef = ClavaJoinPoints.stmtLiteral("#ifndef OFFLOAD");
-        swCall.insertBefore(ifDef);
+        const comment = ClavaJoinPoints.stmtLiteral("// Selector between SW and HW bridge calls based on OFFLOAD getenv variable");
+        bridgeCall.insertBefore(comment);
 
-        const elseDef = ClavaJoinPoints.stmtLiteral("#else");
-        swCall.insertAfter(elseDef);
+        const getenvCall = ClavaJoinPoints.callFromName("getenv", ClavaJoinPoints.type("char*"), ClavaJoinPoints.exprLiteral("\"OFFLOAD\""));
+        const comparison = ClavaJoinPoints.binaryOp("!=", getenvCall, ClavaJoinPoints.exprLiteral("NULL"));
 
-        const endIfDef = ClavaJoinPoints.stmtLiteral("#endif // OFFLOAD");
-        bridgeCall.insertAfter(endIfDef);
+        bridgeCall.detach();
+        const trueScope = ClavaJoinPoints.scope(bridgeCall);
+        trueScope.setNaked(false)
+
+        swCall.detach();
+        const falseScope = ClavaJoinPoints.scope(swCall);
+        falseScope.setNaked(false)
+
+        const ifStmt = ClavaJoinPoints.ifStmt(comparison, trueScope, falseScope);
+        comment.insertAfter(ifStmt);
     }
 
     private buildClusterFunction(clusterFile: FileJp, topFun: FunctionJp, clusterName: string): [FunctionJp, Param[]] {
@@ -164,6 +188,11 @@ export class ClusterOutliner extends AStage {
                 const varName = varref.vardecl.name;
                 const oldGlobal = varref.vardecl.getAncestor("statement") as DeclStmt;
                 const isOnlyRead = this.isOnlyRead(varref, clonedFuns);
+
+                if (oldGlobal == null || oldGlobal === undefined) {
+                    this.logWarning(`  Could not find declaration statement for global variable ${varName}. Skipping.`);
+                    return;
+                }
 
                 if (!globalDecls.has(varName)) {
                     const newGlobal = oldGlobal.deepCopy() as DeclStmt;
@@ -273,24 +302,106 @@ export class ClusterOutliner extends AStage {
 
     private buildSwFunction(cluster: Cluster): [FunctionJp, Call] {
         const tasks = cluster.getTasks();
-        const orderedTasks = TopologicalSort.sort(tasks);
-        const firstTask = orderedTasks[0] as RegularTask;
-        const lastTask = orderedTasks[orderedTasks.length - 1] as RegularTask;
-
         const name = `${cluster.getEntryPointName()}_sw`;
-        const firstCall = firstTask.getCall()!.parent as Statement;
-        const lastCall = lastTask.getCall()!.parent as Statement;
+        let startPoint: Statement | null = null;
+        let endPoint: Statement | null = null;
 
-        if (firstCall === undefined || lastCall === undefined) {
-            throw new Error("[ClusterOutliner] Task calls must have a parent statement.");
+        if (this.isFlatCluster(cluster)) {
+            this.log("  Cluster is flat. Using first and last task calls as outlining boundaries.");
+            const orderedTasks = TopologicalSort.sort(tasks);
+            const firstTask = orderedTasks[0] as RegularTask;
+            const lastTask = orderedTasks[orderedTasks.length - 1] as RegularTask;
+
+            const firstCall = firstTask.getCall()!.parent as Statement;
+            const lastCall = lastTask.getCall()!.parent as Statement;
+
+            if (firstCall === undefined || lastCall === undefined) {
+                throw new Error("[ClusterOutliner] Task calls must have a parent statement.");
+            }
+            startPoint = firstCall;
+            endPoint = lastCall;
+        }
+        else {
+            this.log("  Cluster is non-flat, searching for common ancestor statement");
+            const [stmtChains, minSize] = this.buildStmtChains(cluster);
+            let commonAncestor: Statement | null = null;
+
+            for (let i = 0; i < minSize; i++) {
+                const currentStmts = stmtChains.map((chain) => chain[i]);
+                const firstStmt = currentStmts[0];
+                const allEqual = currentStmts.every((stmt) => stmt.astId === firstStmt.astId);
+                if (allEqual) {
+                    commonAncestor = firstStmt;
+                }
+                else {
+                    break;
+                }
+            }
+            if (commonAncestor === null) {
+                throw new Error("[ClusterOutliner] Could not find a common ancestor statement for outlining");
+            }
+            if (commonAncestor instanceof Loop) {
+                this.log("  Common ancestor is a loop");
+            }
+            else if (commonAncestor instanceof If) {
+                this.log("  Common ancestor is an if statement");
+            }
+            else if (commonAncestor instanceof Switch) {
+                this.log("  Common ancestor is a switch");
+            }
+            else {
+                throw new Error(`[ClusterOutliner] Common ancestor is not a valid outlining statement, was of type ${commonAncestor.joinPointType}`);
+            }
+            startPoint = commonAncestor;
+            endPoint = commonAncestor;
         }
 
+        this.log(`  Outlining cluster region into function ${name}`);
         const outliner = new Outliner();
-        const [newFun, newCall] = outliner.outlineWithName(firstCall, lastCall, name);
+        const [newFun, newCall] = outliner.outlineWithName(startPoint, endPoint, name);
         if (newFun === null || newCall === null) {
             throw new Error("[ClusterOutliner] Outlining failed to produce a new function or call.");
         }
+        this.log(`  Outlined function ${newFun.name} created successfully.`);
         return [newFun, newCall];
+    }
+
+    private isFlatCluster(cluster: Cluster): boolean {
+        const tasks = cluster.getTasks();
+        const scopes = tasks.map((task) => {
+            const call = task.getCall();
+            if (call === null) {
+                throw new Error("[ClusterOutliner] Task does not have an associated call");
+            }
+            return call.getAncestor("scope") as Scope;
+        });
+        return scopes.every((scope) => scope.astId === scopes[0].astId);
+    }
+
+    private buildStmtChains(cluster: Cluster): [Statement[][], number] {
+        const tasks = cluster.getTasks();
+        let smallestChainSize = Number.MAX_SAFE_INTEGER;
+
+        const scopeChains: Statement[][] = tasks.map((task) => {
+            const call = task.getCall();
+            if (call === null) {
+                throw new Error("[ClusterOutliner] Task does not have an associated call");
+            }
+            const chain = [];
+            let currJp = call.parent;
+            while (!(currJp instanceof FunctionJp)) {
+                if (currJp instanceof Statement) {
+                    chain.push(currJp);
+                }
+                currJp = currJp.parent;
+            }
+            chain.reverse();
+            if (chain.length < smallestChainSize) {
+                smallestChainSize = chain.length;
+            }
+            return chain;
+        });
+        return [scopeChains, smallestChainSize];
     }
 
     private renameSwFunction(cluster: Cluster): [FunctionJp, Call] {
